19bd2a5a76d8ca0afae1eb0408aafc15814aa72f
Ahmed Ikram <ahmed@Ahmeds-MacBook-Pro.local>
2025-06-21 19:33:12 +0500

    feat: uploaded files are split into chunks and stored in vector db

--- pages/upload.py
+++ pages/upload.py
@@ -1,4 +1,33 @@
+import os
 import streamlit as st
+
+from pipelines.ingestion import PDFLoader, TextSplitter
+from services.vector_store import VectorManager
+
+def save_files(uploaded_files):
+    data_dir = "data/uploaded_docs"
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    pdf_loader = PDFLoader()
+    text_splitter = TextSplitter().get_text_splitter()
+
+    for uploaded_file in uploaded_files:
+        file_name = uploaded_file.name
+        file_path = os.path.join(data_dir, file_name)
+
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getvalue())
+
+        chunks = pdf_loader.load_and_split(file_path, text_splitter)
+
+        save_to_vector_store(chunks)
+
+def save_to_vector_store(chunks):
+    vector_manager = VectorManager()
+    vector_store = vector_manager.get_vector_store()
+
+    vector_store.add_documents(chunks)
 
 st.title("Document Upload")
 
@@ -13,5 +42,10 @@ with st.form("upload_form", clear_on_submit=True):
         accept_multiple_files=True,
         help="You can select multiple files at once"
     )
-
     save_button = st.form_submit_button("Save", use_container_width=True, type="primary")
+
+    if save_button:
+        if not uploaded_files:
+            st.warning("Please upload at least one file before saving.")
+        else:
+            save_files(uploaded_files)
--- services/vector_store.py
+++ services/vector_store.py
@@ -1,9 +1,22 @@
-from .embeddings import embeddings
 from langchain_chroma import Chroma
+from langchain_chroma.vectorstores import Chroma
+from typing import Optional
 
-vector_store = Chroma(
-    collection_name="compliance_auditor_collection",
-    embedding_function=embeddings,
-    persist_directory="../data/chromadb",
-)
+from .embeddings import EmbeddingService
+
+class VectorManager:
+    def __init__(self):
+        self.vector_store: Optional[Chroma] = None
+
+    def get_vector_store(self) -> Chroma:
+        if not self.vector_store:
+            embeddings = EmbeddingService().get_embedding_model()
+
+            self.vector_store = Chroma(
+                collection_name="compliance_auditor_collection",
+                embedding_function=embeddings,
+                persist_directory="../data/chromadb",
+            )
+
+        return self.vector_store
 
